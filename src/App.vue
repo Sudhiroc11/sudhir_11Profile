8bc0f0b985138981cf08f48910d36498b614bb97
sudhir <sudhir.oc11@gmail.com>
2022-05-13 00:26:24 -0700

    kk

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf" id="lay-out">
-    <notifications group="foo" position="top center" style="z-index: 10001" />
     <router-view /> 
   </q-layout>
 </template>
@@ -15,8 +14,4 @@ export default{
 </script>
 
 <style>
-#lay-out{
-  overflow-x: auto;
-  height: 100vh;
-}
 </style>
