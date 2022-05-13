0276363059dd716d10593301e77757cfe0a7a46b
sudhir <sudhir.oc11@gmail.com>
2022-05-13 00:08:46 -0700

    dd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,204 +1,269 @@
 <template>
   <div>
-    <div class="desktop-view">
-      <div class="row">
-        <div class="col-lg-3 col-md-3">
-          <q-drawer
-            v-model="leftDrawerOpen"
-            bordered
-            :width="400"
-            :content-style="{ backgroundColor: '#0C365A' }"
+    <div>
+      <div class="col-lg-1 col-md-1 col-sm-1"></div>
+      <div class="col-lg-10 col-md-10 col-sm-10">
+        <div class="choice-container">
+          <div class="user-text-name text-h5">
+            <div>
+              <b> Let us know you better</b>
+            </div>
+            <div class="q-px-sm">
+              <div class="hi-icon"></div>
+            </div>
+          </div>
+          <div class="q-py-sm" style="color:grey;">
+            Pick 3 (or more!) topics of your choice
+          </div>
+          <q-scroll-area
+            :thumb-style="thumbStyle"
+            :bar-style="barStyle"
+            style="height: 450px; max-width: 100%;"
           >
-            <div class="q-px-lg">
-              <img
-                src="../assets/Image/aspireLogo.svg"
-                class="q-mt-xl q-mb-lg"
-              />
-              <div style="color:#FFFFFF;opacity:30%;">
-                Trusted way of banking for 
-                3,000+ SMEs and startups in Singapore
+            <div class="row">
+              <div class="col-lg-4 col-md-4 col-sm-4 q-pr-md q-py-sm"
+                v-for='job in jobs' :key='job.jobId'
+              >
+                <q-card class="student-choice-card">
+                  <q-checkbox 
+                    v-model="job.jobId" 
+                    color="green" 
+                  />
+
+
+                  <div class="q-px-sm">
+                    Selected professioan: <strong>{{ job.jobId }}</strong>
+                  </div>
+                  <div class="text-h6" style="text-align:center;padding-top:30px;">
+                    {{job.jobName}}
+                  </div>
+                </q-card>
               </div>
             </div>
-
-            <q-list class="q-pt-lg">
-
-              <q-item clickable v-ripple style="color: white;" class="q-py-lg">
-                <q-item-section avatar>
-                  <q-icone>
-                    <img src="../assets/Image/Home.png">
-                  </q-icone>
-                </q-item-section>
-                <q-item-section>Home</q-item-section>
-              </q-item>
-
-              <q-item clickable v-ripple style="color: green;" class="q-py-lg">
-                <q-item-section avatar>
-                  <q-icone>
-                    <img src="../assets/Image/Card.png">
-                  </q-icone>
-                </q-item-section>
-                <q-item-section>Cards</q-item-section>
-              </q-item>
-
-              <q-item clickable v-ripple style="color: white;" class="q-py-lg">
-                <q-item-section avatar>
-                  <q-icone>
-                    <img src="../assets/Image/Payments.png">
-                  </q-icone>
-                </q-item-section>
-                <q-item-section>Payments</q-item-section>
-              </q-item>
-
-
-              <q-item clickable v-ripple style="color: white;" class="q-py-lg">
-                <q-item-section avatar>
-                  <q-icone>
-                    <img src="../assets/Image/Credit.png">
-                  </q-icone>
-                </q-item-section>
-                <q-item-section>Credit</q-item-section>
-              </q-item>
-
-              <q-item clickable v-ripple style="color: white;" class="q-py-lg">
-                <q-item-section avatar>
-                  <q-icone>
-                    <img src="../assets/Image/Account.png">
-                  </q-icone>
-                </q-item-section>
-                <q-item-section>Settings</q-item-section>
-              </q-item>
-
-            </q-list>
-          </q-drawer>
-        </div>
-        <div class="col-lg-1 col-md-1">
-        </div>
-        <div class="col-lg-8 col-md-8">
-          <div>
-           <userCard />
+          </q-scroll-area>
+          <div class="get-started-div q-pa-lg">
+            <q-btn
+              label="get started" 
+              class="get-started" 
+            />
           </div>
         </div>
       </div>
+      <div class="col-lg-1 col-md-1 col-sm-1"></div>
     </div>
-    <div class="mobile-view">
-      <q-header class="aspire-mb-header">
-        <q-toolbar class="q-px-lg q-mt-md">
-          <q-space />
-          <img
-            src="../assets/Image/aspireIcone.png"
-          />
-        </q-toolbar>
-        <div>
-          <userCard />
-        </div>
-      </q-header>
-      <q-page class="aspire-page">
-        <div v-for="n in 100" :key="n" class="q-pa-xs">
-          Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit, sed do eiusmod tempor incididunt ut labore et
-          dolore magna aliqua.
-        </div>
-      </q-page>
-      <q-footer elevated class="bg-white text-primary">
-        <div class="aspire-mb-footer q-py-sm">
-          <div>
-            <div>
-              <img src="../assets/Image/Logomb.png">
-            </div>
-            <div style="color:#DDDDDD;">Home</div>
-          </div>
-          <div>
-            <div>
-              <img src="../assets/Image/Card.png">
-            </div>
-            <div style="color:#01D167;">Cards</div>
-          </div>
-          <div>
-            <div>
-              <img src="../assets/Image/Payments.png">
-            </div>
-            <div style="color:#DDDDDD;">Payments</div>
-          </div>
-          <div>
-            <div>
-              <img src="../assets/Image/Creditmb.png">
-            </div>
-            <div style="color:#DDDDDD;">Credit</div>
-          </div>
-          <div>
-            <div>
-              <img src="../assets/Image/Accountmb.png">
-            </div>
-            <div style="color:#DDDDDD;">Settings</div>
-          </div>
-        </div>
-      </q-footer>
-    </div>
-    <q-page-container>
-      <transition name="slide" mode="out-in">
-			  <router-view />
-      </transition>
-    </q-page-container>
   </div>
 </template>
 
 <script>
-import userCard from '../components/userCard';
 export default {
-  components: {
-    userCard,
+  computed: {
+    
   },
-  data () {
+  data() {
     return {
-      leftDrawerOpen: true,
-      tab: 'images',
-    }
+      jobs:[
+        {
+          jobName: "Engineer",
+          jobId: 1,
+        },
+        {
+          jobName: "Doctor",
+          jobId: 2,
+        },
+        {
+          jobName: "Pilot",
+          jobId: 3,
+        },
+        {
+          jobName: "Software Devloper",
+          jobId: 4,
+        },
+        {
+          jobName: "Artist",
+          jobId: 5,
+        },
+        {
+          jobName: "Teacher",
+          jobId: 6,
+        },
+        {
+          jobName: "CA",
+          jobId: 7,
+        },
+        {
+          jobName: "MBA",
+          jobId: 8,
+        },
+         {
+          jobName: "Engineer",
+          jobId: 9,
+        },
+        {
+          jobName: "Doctor",
+          jobId: 10,
+        },
+        {
+          jobName: "Pilot",
+          jobId: 11,
+        },
+        {
+          jobName: "Software Devloper",
+          jobId: 12,
+        },
+        {
+          jobName: "Artist",
+          jobId: 13,
+        },
+        {
+          jobName: "Teacher",
+          jobId: 14,
+        },
+        {
+          jobName: "CA",
+          jobId: 15,
+        },
+        {
+          jobName: "MBA",
+          jobId: 16,
+        },
+        {
+          jobName: "MBA",
+          jobId: 17,
+        },
+        {
+          jobName: "Artist",
+          jobId: 18,
+        },
+        {
+          jobName: "Teacher",
+          jobId: 19,
+        },
+        {
+          jobName: "Engineer",
+          jobId: 20,
+        },
+        {
+          jobName: "Doctor",
+          jobId: 21,
+        },
+        {
+          jobName: "Pilot",
+          jobId: 22,
+        },
+        {
+          jobName: "Software Devloper",
+          jobId: 23,
+        },
+        {
+          jobName: "Artist",
+          jobId: 24,
+        },
+        {
+          jobName: "Teacher",
+          jobId: 25,
+        },
+        {
+          jobName: "CA",
+          jobId: 26,
+        },
+      ],
+      thumbStyle: {
+        right: '4px',
+        borderRadius: '5px',
+        backgroundColor: '#303C6E',
+        width: '5px',
+        opacity: 0.75
+      },
+      barStyle: {
+        right: '2px',
+        borderRadius: '9px',
+        backgroundColor: '#303C6E',
+        width: '9px',
+        opacity: 0.2
+      },
+    };
   },
-}
+  methods:{
+
+  },
+};
 </script>
+
 <style lang="scss" scoped>
-$blue-color:#0C365A;
-$green-color:#01D167;
-.text-aspire-blue {
-  color: $blue-color !important;
+$blue-color:#34568B;
+$black-color:#00160A;
+.left-screen{
+  min-height: 100vh;
+  background-color: #f6f7f8;
 }
-.bg-aspire-blue {
-  background: $blue-color !important;
+.cursor-text{
+  cursor: pointer;
+  color: $black-color;
+}
+.cursor-text:hover{
+  color: orange;
+  cursor: pointer;
+}
+.bottom-home{
+  position: absolute;
+  bottom: 4%;
+  display: flex;
+}
+.copy-right{
+  position: absolute;
+  bottom: 4%;
+  right: 52%;
+  color: $black-color;
+}
+.copy-right1{
+  position: absolute;
+  bottom: 0;
+  color: $black-color;
 }
 @media all and (max-width: 1023px){
-  .mobile-view{
+  .copy-right1{
     display: block;
   }
-  .desktop-view{
+  .copy-right{
     display: none;
   }
 }
-@media all and (min-width: 1024px){
-  .mobile-view{
+@media all and (min-width: 1022px){
+  .copy-right1{
     display: none;
   }
-  .desktop-view{
+  .copy-right{
     display: block;
   }
 }
-.aspire-mb-header{
-  background-color: $blue-color;
-  color: #fff;
-  padding-bottom: 3rem;
-  z-index: 0;
-}
-.aspire-mb-footer{
+//Right screen
+.user-text-name{
   display: flex;
-  text-align: center;
-  justify-content: space-around;
 }
-.aspire-page{
-  border-top-left-radius: 1.5rem;
-  border-top-right-radius: 1.5rem;
-  margin-top: -1rem;
-  background: #fff;
-  padding: 1.5rem;
-  margin-top: 450px;
+.get-started{
+  background: $blue-color !important;
+  color: white !important;
+}
+.student-choice-card{
+  background:#34568B;
+  color:white;
+  height:135px;
+  cursor: pointer;
+}
+
+.get-started-div{
+  text-align: center;
+}
+.hi-icon{
+  background-repeat: no-repeat;
+  background-size: 28px;
+  height: 30px;
+  width: 35px;
+}
+.choice-container{
+  position: absolute;
+  top: 10%;
+  left: 53%;
+  right: 3%;
 }
 </style>
-
